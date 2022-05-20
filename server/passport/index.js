4bfd0c8fd5904cb2b1d85452035b361ba1afae8c
synodical <lena0323park@naver.com>
2022-05-20 13:32:29 +0900

    Feat: 회원가입과 로그인 기능

---
+++ server/passport/index.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const local = require("./localStrategy");
+const Customer = require("../models").Customer;
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({ where: { id } })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+
+  local();
+};
