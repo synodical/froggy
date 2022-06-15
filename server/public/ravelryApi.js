0016453fc418842e03084764987080fd963146b2
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-06-15 20:20:08 +0900

    랜덤이미지삽입
    랜덤으로 메인 페이지의 패턴과 실을 보여줍니다.

--- server/public/ravelryApi.js
+++ server/public/ravelryApi.js
@@ -1,62 +1,66 @@
+const BASE_URI = "https://api.ravelry.com";
+const headers = new Headers(); // 인증
+headers.append(
+  "Authorization",
+  "Basic " +
+    btoa(
+      "read-ea6b8e0a978e097b8badec466c71a60b" +
+        ":" +
+        "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
+    )
+);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
 async function getPatternImgList() {
-  imageUrls = [];
-  try {
-    for (let i = 1234; i < 1239; i++) {
-      const BASE_URI = "https://api.ravelry.com";
-      const endpoint = `${BASE_URI}/patterns/${i}.json`;
-      const headers = new Headers(); // 인증
-      headers.append(
-        "Authorization",
-        "Basic " +
-          btoa(
-            "read-ea6b8e0a978e097b8badec466c71a60b" +
-              ":" +
-              "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
-          )
-      );
+  let mainImgNum = 5;
+  let iter = 0;
+  let endpoint;
+  let tmpId;
+  while (iter < mainImgNum) {
+    try {
+      tmpId = getRandomInt(1, 10000);
+      endpoint = `${BASE_URI}/patterns/${tmpId}.json`;
       fetch(endpoint, { method: "GET", headers: headers })
         .then((res) => res.json())
         .then((data) => {
           let url = data.pattern.photos[0].small_url;
-          let id = data.pattern.id;
           const pattern_section = document.getElementById("pattern_section");
-          pattern_section.innerHTML += `<img src="${url}" alt="pic of pattern" class="mx-1 my-2" height="160" id="patternImageUrl${id}" />`;
+          pattern_section.innerHTML += `<img src="${url}" alt="pic of pattern" class="mx-1 my-2" height="160" id="patternImageUrl${tmpId}" />`;
         })
         .catch((err) => alert(err));
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
+    iter++;
   }
 }
 
 async function getYarnImgList() {
-  imageUrls = [];
-  try {
-    for (let i = 560; i < 570; i++) {
-      const BASE_URI = "https://api.ravelry.com";
-      const endpoint = `${BASE_URI}/yarns/${i}.json`;
-      const headers = new Headers(); // 인증
-      headers.append(
-        "Authorization",
-        "Basic " +
-          btoa(
-            "read-ea6b8e0a978e097b8badec466c71a60b" +
-              ":" +
-              "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
-          )
-      );
+  let mainImgNum = 5;
+  let iter = 0;
+  let endpoint;
+  let tmpId;
+  while (iter < mainImgNum) {
+    try {
+      tmpId = getRandomInt(1, 10000);
+      endpoint = `${BASE_URI}/yarns/${tmpId}.json`;
       fetch(endpoint, { method: "GET", headers: headers })
         .then((res) => res.json())
         .then((data) => {
           let url = data.yarn.photos[0].small_url;
-          let id = data.yarn.id;
           const yarn_section = document.getElementById("yarn_section");
-          yarn_section.innerHTML += `<img src="${url}" alt="pic of yarn" class="mx-1 my-2" height="160" id="yarnImageUrl${id}" />`;
+          yarn_section.innerHTML += `<img src="${url}" alt="pic of yarn" class="mx-1 my-2" height="160" id="yarnImageUrl${tmpId}}" />`;
         })
         .catch((err) => alert(err));
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
+    iter++;
   }
 }
 
