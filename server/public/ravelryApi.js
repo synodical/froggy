31aa9aa418593265016b8917555afdc4c9fe6af0
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-06-15 19:37:35 +0900

    main화면 추천 yarn 추가하기

--- server/public/ravelryApi.js
+++ server/public/ravelryApi.js
@@ -17,11 +17,10 @@ async function getPatternImgList() {
       fetch(endpoint, { method: "GET", headers: headers })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data.pattern.id);
-          let url = data.pattern.photos[0].square_url;
+          let url = data.pattern.photos[0].small_url;
           let id = data.pattern.id;
           const pattern_section = document.getElementById("pattern_section");
-          pattern_section.innerHTML += `<img src="${url}" alt="pics of yarn" width="180" height="180" id="yarnImageUrl${id}" />`;
+          pattern_section.innerHTML += `<img src="${url}" alt="pic of pattern" class="mx-1 my-2" height="160" id="patternImageUrl${id}" />`;
         })
         .catch((err) => alert(err));
     }
@@ -30,29 +29,32 @@ async function getPatternImgList() {
   }
 }
 
-async function getImg(num) {
+async function getYarnImgList() {
+  imageUrls = [];
   try {
-    let img;
-    const BASE_URI = "https://api.ravelry.com";
-    const endpoint = `${BASE_URI}/patterns/${num + 1234}.json`;
-    //인증을 위한 헤더
-    const headers = new Headers();
-    headers.append(
-      "Authorization",
-      "Basic " +
-        btoa(
-          "read-ea6b8e0a978e097b8badec466c71a60b" +
-            ":" +
-            "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
-        )
-    );
-    fetch(endpoint, { method: "GET", headers: headers })
-      .then((res) => res.json())
-      .then((data) => {
-        img = data;
-        console.log("fetch 종료1", img);
-      })
-      .catch((err) => alert(err));
+    for (let i = 560; i < 570; i++) {
+      const BASE_URI = "https://api.ravelry.com";
+      const endpoint = `${BASE_URI}/yarns/${i}.json`;
+      const headers = new Headers(); // 인증
+      headers.append(
+        "Authorization",
+        "Basic " +
+          btoa(
+            "read-ea6b8e0a978e097b8badec466c71a60b" +
+              ":" +
+              "nKTdyxY5v3mF3TDb8hkQHGhHwHNQL7lbJtU8Dz15"
+          )
+      );
+      fetch(endpoint, { method: "GET", headers: headers })
+        .then((res) => res.json())
+        .then((data) => {
+          let url = data.yarn.photos[0].small_url;
+          let id = data.yarn.id;
+          const yarn_section = document.getElementById("yarn_section");
+          yarn_section.innerHTML += `<img src="${url}" alt="pic of yarn" class="mx-1 my-2" height="160" id="yarnImageUrl${id}" />`;
+        })
+        .catch((err) => alert(err));
+    }
   } catch (err) {
     console.error(err);
   }
