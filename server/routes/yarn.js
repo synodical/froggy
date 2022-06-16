93219540638b14c6eb05d7de3fc64458199be9e6
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-06-16 14:54:00 +0900

    검색창제작

--- server/routes/yarn.js
+++ server/routes/yarn.js
@@ -6,10 +6,6 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
-  let urls = [];
-  for (let i = 0; i < 4; i++) {
-    urls.push(imgs.data[i].download_url);
-  }
   res.render("yarn.html", {
     title: "froggy",
     customer: req.user,
