bd4164b2f114df98157b76af576a4d53012d825b
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-08 23:17:11 +0900

    yarn row create

--- server/routes/dbTest.js
+++ server/routes/dbTest.js
@@ -4,10 +4,43 @@ const Yarn = require("../models").Yarn;
 const Pattern = require("../models").Pattern;
 const router = express.Router();
 const axios = require("axios");
+const Customer = require("../models").Customer;
+const models = require("../models");
 
 router.post("/", async (req, res, next) => {
   console.log(req.body);
-  const { yarnData } = req.body;
+  const { yarn } = req.body;
+
+  if (yarn === undefined) {
+    return res.status(400);
+  }
+  const gauge_divisor = yarn.gauge_divisor;
+  const grams = yarn.grams;
+  const id = yarn.id;
+  const machine_washable = yarn.machine_washable;
+  const name = yarn.name;
+  const yardage = yarn.yardage;
+  const yarn_company_name = yarn.yarn_company_name;
+  try {
+    const yarnId = yarn.id;
+    const exYarn = await Yarn.findOne({ where: { id: yarnId } });
+    if (exYarn) {
+      return res.redirect("/join?error=exist");
+    }
+    await Yarn.create({
+      gauge_divisor: gauge_divisor,
+      grams: grams,
+      id: id,
+      machine_washable: machine_washable,
+      name: name,
+      yardage: yardage,
+      yarn_company_name: yarn_company_name,
+    });
+    return res.status(200);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 module.exports = router;
