fbac9e8eaa825a0ff52ec0563fb03a585536ccae
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-08 23:16:29 +0900

    model table option 추가하기

--- server/models/yarn.js
+++ server/models/yarn.js
@@ -1,29 +1,36 @@
-module.exports = (sequelize, DataTypes) => {
-  sequelize.define("yarn", {
-    gauge_divisor: {
-      type: DataTypes.INTEGER,
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "yarn",
+    {
+      gauge_divisor: {
+        type: DataTypes.INTEGER,
+      },
+      grams: {
+        type: DataTypes.INTEGER,
+      },
+      id: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      machine_washable: {
+        type: DataTypes.BOOLEAN,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNULL: false,
+      },
+      yardage: {
+        type: DataTypes.INTEGER,
+      },
+      yarn_company_name: {
+        type: DataTypes.STRING(50),
+      },
     },
-    grams: {
-      type: DataTypes.INTEGER,
-    },
-    id: {
-      type: DataTypes.STRING(40),
-      allowNull: false,
-      unique: true,
-      primaryKey: true,
-    },
-    machine_washable: {
-      type: DataTypes.BOOLEAN,
-    },
-    name: {
-      type: DataTypes.STRING(50),
-      allowNULL: false,
-    },
-    yardage: {
-      type: DataTypes.INTEGER,
-    },
-    yarn_company_name: {
-      type: DataTypes.STRING(50),
-    },
-  });
-};
+    {
+      underscored: true,
+      freezeTableName: true,
+      tableName: "yarn",
+    }
+  );
