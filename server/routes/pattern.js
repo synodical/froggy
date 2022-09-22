90699450645c12f07eb27f19016b8b34d1ab7ea8
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-22 18:11:39 +0900

    random한 pattern을 json형식으로 프론트에 주기

--- server/routes/pattern.js
+++ server/routes/pattern.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const { request } = require("http");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const Customer = require("../models").Customer;
 const router = express.Router();
-const axios = require("axios");
+const { Pattern } = require("../models");
+const Sequelize = require("sequelize");
 
 router.get("/", async (req, res, next) => {
-  res.render("pattern.html", {
-    title: "froggy",
-    customer: req.user,
-    loginError: req.flash("loginError"),
-  });
+  // res.render("pattern.html", {
+  //   title: "froggy",
+  //   customer: req.user,
+  //   loginError: req.flash("loginError"),
+  // });
+  let resJson = { status: "N" };
+  try {
+    const randPattern = await Pattern.findAll({
+      // attributes: ["id"],
+      order: Sequelize.fn("RAND"),
+      limit: 5, // limit으로 반환받을 row 수를 정할 수 있어요
+    });
+    resJson["status"] = "Y";
+    // console.log(randPattern);
+    return res.json(randPattern);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 module.exports = router;
