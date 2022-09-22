a93663da4ce77a3525b1af54e0582c2af2ea7f07
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-22 18:14:35 +0900

    random한 Yarn을 json으로 프론트에 주기

--- server/routes/yarn.js
+++ server/routes/yarn.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const { request } = require("http");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const Customer = require("../models").Customer;
 const Yarn = require("../models").Yarn;
 const router = express.Router();
 const axios = require("axios");
+const Sequelize = require("sequelize");
 
 router.get("/", async (req, res, next) => {
-  res.render("yarn.html", {
-    title: "froggy",
-    customer: req.user,
-    loginError: req.flash("loginError"),
-  });
+  // res.render("yarn.html", {
+  //   title: "froggy",
+  //   customer: req.user,
+  //   loginError: req.flash("loginError"),
+  // });
+  let resJson = { status: "N" };
+  try {
+    const randYarn = await Yarn.findAll({
+      // attributes: ["id"],
+      order: Sequelize.fn("RAND"),
+      limit: 5, // limit으로 반환받을 row 수를 정할 수 있어요
+    });
+    resJson["status"] = "Y";
+    return res.json(randYarn);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 module.exports = router;
