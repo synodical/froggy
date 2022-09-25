7664d1b7ae0822b25fda09a593251d189f4cb697
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-25 15:01:40 +0900

    images json으로 전송

--- server/routes/pattern.js
+++ server/routes/pattern.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { request } = require("http");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const router = express.Router();
-const { Pattern } = require("../models");
+const { Pattern, Customer } = require("../models");
 const Sequelize = require("sequelize");
 
 router.get("/", async (req, res, next) => {
@@ -13,9 +13,30 @@ router.get("/", async (req, res, next) => {
       order: Sequelize.fn("RAND"),
       limit: 5, // limit으로 반환받을 row 수를 정할 수 있어요
     });
+    let Images = [];
+    randPattern.forEach((element) => {
+      const eachImage = Image.findOne({
+        attributes: ["squareUrl"],
+        where: {
+          targetType: "Pattern",
+          targetId: element.id,
+        },
+      });
+      Images.push(eachImage);
+    });
+    resJson["images"] = Images;
     resJson["randPattern"] = randPattern;
     resJson["status"] = "Y";
     return res.json(randPattern);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.post("/:id/liked", async (req, res, next) => {
+  try {
+    Customer.addLikedPattern(Pattern, { through: { status: "started" } });
   } catch (error) {
     console.error(error);
     return next(error);
