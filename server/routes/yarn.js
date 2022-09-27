f3c5c021e803a02a5ac016ae82d409ed73bb168c
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-28 00:25:19 +0900

    yarn에 검색 라우터와 기능 추가하기

--- server/routes/yarn.js
+++ server/routes/yarn.js
@@ -1,11 +1,36 @@
 const express = require("express");
-const { request } = require("http");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const Yarn = require("../models").Yarn;
 const router = express.Router();
-const axios = require("axios");
-const { Pattern, Customer, Image } = require("../models");
+const { Pattern, Customer, Image, sequelize, Yarn } = require("../models");
 const Sequelize = require("sequelize");
+const { QueryTypes } = require("sequelize");
+
+router.get("/search", async (req, res, next) => {
+  let keyword = req.query[0];
+  console.log(keyword);
+  keyword = keyword.toString().replace(" ", "%"); // db에는 빨간실로 저장되어 있지만, 빨간 실로 검색한 경우
+  // keyword = `%${keyword.replace(/ /gi, "%")}%`;
+  const query = `select * from yarn where name like '%${keyword}%' or yarnCompanyName like '%${keyword}%'`;
+  try {
+    const searchList = await sequelize.query(query, {
+      replacements: { keyword: keyword },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    if (searchList.length == 0) {
+      res.status(204).json({
+        message: "No results or fail",
+      });
+    } else {
+      res.status(200).json({
+        searchList: searchList,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+});
 
 router.get("/:id", async (req, res, next) => {
   let resJson = { status: "N" };
