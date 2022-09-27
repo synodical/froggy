4585c1da3f94eac818206a2b7fce4658f5416fe4
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-09-28 00:24:45 +0900

    패턴 라우터의 서브 주소로 검색 주소 병합

--- server/routes/pattern.js
+++ server/routes/pattern.js
@@ -2,8 +2,38 @@ const express = require("express");
 const { request } = require("http");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const router = express.Router();
-const { Pattern, Customer, Image } = require("../models");
+const { Pattern, Customer, Image, sequelize } = require("../models");
 const Sequelize = require("sequelize");
+const { QueryTypes } = require("sequelize");
+
+router.get("/search", async (req, res, next) => {
+  let keyword = req.query[0];
+  console.log(keyword);
+  keyword = keyword.toString().replace(" ", "%"); // db에는 빨간실로 저장되어 있지만, 빨간 실로 검색한 경우
+  // keyword = `%${keyword.replace(/ /gi, "%")}%`;
+  const query =
+    // 'select * from pattern where replace(name," ","") like :keyword or replace(author," ","") like :keyword';
+    `select * from pattern where name like '%${keyword}%' or author like '%${keyword}%'`;
+  try {
+    const searchList = await sequelize.query(query, {
+      replacements: { keyword: keyword },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    if (searchList.length == 0) {
+      res.status(204).json({
+        message: "No results or fail",
+      });
+    } else {
+      res.status(200).json({
+        searchList: searchList,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+});
 
 router.post("/:id/liked", async (req, res, next) => {
   try {
@@ -57,9 +87,9 @@ router.get("/", async (req, res, next) => {
         raw: true,
       });
       if (eachImage === null) {
-        ry["thumbnail"] = null;
+        rp["thumbnail"] = null;
       } else {
-        ry["thumbnail"] = eachImage.mediumUrl; // null일때 예외처리하기
+        rp["thumbnail"] = eachImage.mediumUrl; // null일때 예외처리하기
       }
     }
     resJson["patternList"] = randPattern;
