aa7c6ab030bbc6e042939e8d05d6e96a0daf1554
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-10-16 18:14:13 +0900

    liked model 생성하기

--- server/models/liked.js
+++ server/models/liked.js
@@ -1,31 +1,25 @@
-// module.exports = (sequelize, DataTypes) =>
-//   sequelize.define(
-//     "liked",
-//     {
-//       id: {
-//         type: DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true,
-//       },
-//       targetType: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         defaultValue: "",
-//       },
-//       targetId: {
-//         type: DataTypes.BIGINT,
-//       },
-//       customerId: {
-// id: {
-//   type: sequelize.INTEGER,
-//   autoIncrement: true,
-//   primaryKey: true,
-// },
-//       },
-//     },
-//     {
-//       freezeTableName: true,
-//       paranoid: true.valueOf,
-//       tableName: "liked",
-//     }
-//   );
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "liked",
+    {
+      targetType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "",
+        primaryKey: true,
+      },
+      targetId: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+    },
+    {
+      freezeTableName: true,
+      paranoid: true.valueOf,
+      tableName: "liked",
+    }
+  );
