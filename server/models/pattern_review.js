61781bb001ba90124bc7841e9885cadfa5f447c1
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-11-12 18:08:45 +0900

    pattern model 생성

---
+++ server/models/pattern_review.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "patternReview",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      patternId: {
+        type: DataTypes.STRING(40),
+        allowNULL: false,
+      },
+      userId: {
+        type: DataTypes.STRING(40),
+        allowNULL: false,
+      },
+      userNick: {
+        type: DataTypes.STRING(20),
+        allowNULL: false,
+      },
+      contents: {
+        type: DataTypes.STRING(4000),
+        allowNULL: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      freezeTableName: true,
+      paranoid: true.valueOf,
+      tableName: "patternReview",
+    }
+  );
