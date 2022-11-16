f707bdb853ba6c797c2fcfe19ecf5386bf64a839
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-11-16 21:56:32 +0900

    isDel column 삭제

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,54 +1,49 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define(
     "comment",
-      {
-          id: {
-              type: DataTypes.INTEGER,
-              autoIncrement: true,
-              primaryKey: true,
-          },
-          postId: {
-              type: DataTypes.INTEGER,
-              allowNULL: false,
-          },
-          userId: {
-              type: DataTypes.STRING(40),
-              allowNULL: false,
-          },
-          userNick: {
-              type: DataTypes.STRING(20),
-              allowNULL: false,
-          },
-          contents: {
-              type: DataTypes.STRING(1000),
-              allowNULL: false,
-          },
-          createdAt: {
-              type: DataTypes.DATE,
-          },
-          depth: {
-              type: DataTypes.INTEGER,
-              allowNULL: false,
-              defaultValue: 0,
-          },
-          bundleId: {
-              type: DataTypes.INTEGER,
-          },
-          bundleOrder: {
-              type: DataTypes.INTEGER,
-          },
-          updatedAt: {
-              type: DataTypes.DATE,
-          },
-          isDel: {
-              type: DataTypes.STRING,
-              allowNULL: false,
-              defaultValue: 'N',
-          },
-          deletedAt: {
-              type: DataTypes.DATE,
-          },
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
+      postId: {
+        type: DataTypes.INTEGER,
+        allowNULL: false,
+      },
+      userId: {
+        type: DataTypes.STRING(40),
+        allowNULL: false,
+      },
+      userNick: {
+        type: DataTypes.STRING(20),
+        allowNULL: false,
+      },
+      contents: {
+        type: DataTypes.STRING(1000),
+        allowNULL: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      depth: {
+        type: DataTypes.INTEGER,
+        allowNULL: false,
+        defaultValue: 0,
+      },
+      bundleId: {
+        type: DataTypes.INTEGER,
+      },
+      bundleOrder: {
+        type: DataTypes.INTEGER,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+      },
+    },
     {
       freezeTableName: true,
       paranoid: true.valueOf,
