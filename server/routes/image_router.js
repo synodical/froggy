109ad02c854d033c9b79152b8f739f6613445d61
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-18 16:59:05 +0900

    image try catch

--- server/routes/image_router.js
+++ server/routes/image_router.js
@@ -4,6 +4,7 @@ const multer = require("multer"); //
 const FirebaseStorageService = require("../common/firebase_storage_service");
 const CommonService = require("../common/common_service");
 const UserController = require("../controllers/user_controller");
+const sharp = require("sharp");
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -12,18 +13,21 @@ const upload = multer({
   },
 });
 router.post("/single", upload.single("image"), async (req, res, next) => {
-  let resJson = { status: "N" };
-  const { file } = req.body;
-  const user = req.user;
-  if (CommonService.isEmpty(user)) {
-    resJson["isUserLogin"] = "N";
-    resJson["reason"] = "다시 로그인 해주세요";
+  try {
+    let resJson = { status: "N" };
+    const { file } = req.body;
+    const user = req.user;
+    if (CommonService.isEmpty(user)) {
+      resJson["isUserLogin"] = "N";
+      resJson["reason"] = "다시 로그인 해주세요";
+      return res.json(resJson);
+    }
+    const imageUrlList = await FirebaseStorageService.uploadPhotos(req, res);
+    resJson["imageUrlList"] = imageUrlList;
+    resJson["status"] = "Y";
     return res.json(resJson);
+  } catch (error) {
+    console.log(error);
   }
-  const imageUrlList = await FirebaseStorageService.uploadPhotos(req, res);
-
-  resJson["imageUrlList"] = imageUrlList;
-  resJson["status"] = "Y";
-  return res.json(resJson);
 });
 module.exports = router;
