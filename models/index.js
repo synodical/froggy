0ce0145b80aa9f8d8646c99523f3924a0f0d849c
LeeYeEun <LeeYeEun@LeeYeEunui-MacBookPro.local>
2022-11-21 15:41:24 +0900

    다시 env로.. db연결

--- models/index.js
+++ models/index.js
@@ -4,18 +4,18 @@ const config = require("../config/config.js")[env];
 
 const db = {};
 const sequelize = new Sequelize(
-  // process.env.MYSQL_DATABASE,
-  // process.env.MYSQL_USERNAME,
-  // process.env.MYSQL_PASSWORD,
-  // {
-  //   host: process.env.MYSQL_HOST,
-  //   dialect: "mysql",
-  // }
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USERNAME,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  }
 
-  config.database,
-  config.username,
-  config.password,
-  config
+  // config.database,
+  // config.username,
+  // config.password,
+  // config
 );
 
 console.log(sequelize);
