36f46065450e5efe4468f10cd2b7842b7b73c163
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-22 14:49:03 +0900

    cookie error fix

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ app.use(
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-      domain: "froggy-synodical.koyeb.app",
       sameSite: "NONE",
       secure: "true",
     },
@@ -88,15 +87,15 @@ const corsOptions = {
 //cors에 옵션사용할경우
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://froggy-34371.web.app");
-  res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "https://froggy-34371.web.app");
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use("/", pageRouter);
 app.use("/auth", authRouter);
