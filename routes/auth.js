5debeac5d03661dd066d79edd4cfb614bb2b7218
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-23 22:59:50 +0900

    change code List

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const models = require("../models");
 const router = express.Router();
-const CODE_LIST = ["yeohon", "somsom"];
+const CODE_LIST = ["frog", "somsom", "ayoung"];
 
 router.post("/join", isNotLoggedIn, async (req, res, next) => {
   let respJson = { status: "N" };
